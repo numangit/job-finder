17998a7bc63c378b8924692fb5cb765c91470669
numangit <abdulnumanjob@gmail.com>
2023-03-13 23:11:16 +0600

    update and post api for job slice created

--- src/features/jobs/jobsAPI.js
+++ src/features/jobs/jobsAPI.js
@@ -1,5 +1,25 @@
-export const fetchJob = () => {
+export const getJobs = () => {
     const response = fetch("http://localhost:9000/jobs");
+    const data = response.json();
+    return data;
+};
+
+export const postJob = (jobData) => {
+    const response = fetch("http://localhost:9000/jobs", {
+        method: 'POST',
+        headers: { "content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify(jobData)
+    });
+    const data = response.json();
+    return data;
+};
+
+export const updateJob = (id, jobData) => {
+    const response = fetch(`http://localhost:9000/jobs/${id}`, {
+        method: 'PATCH',
+        headers: { "content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify(jobData)
+    });
     const data = response.json();
     return data;
 };
