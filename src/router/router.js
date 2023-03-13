6c8921e7aaa5bfe24bb49325f2901679c2cbe4dc
numangit <abdulnumanjob@gmail.com>
2023-03-13 22:32:44 +0600

    router paths set

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import AddJob from "../pages/AddJob";
+import EditJob from "../pages/EditJob";
 import Home from "../pages/Home";
 
 export const router = createBrowserRouter([
@@ -11,6 +13,14 @@ export const router = createBrowserRouter([
                 path: '/',
                 element: <Home />
             },
+            {
+                path: '/addJobs',
+                element: <AddJob />
+            },
+            {
+                path: '/editJobs/:id',
+                element: <EditJob />,
+            }
         ]
     }
 ])
