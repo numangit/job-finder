faf37bfae1242831dd2a83b691414ba544dcb01d
numangit <abdulnumanjob@gmail.com>
2023-03-13 23:25:55 +0600

    thunk function for add, edit, delete added

--- src/features/jobs/jobsSlice.js
+++ src/features/jobs/jobsSlice.js
@@ -1,4 +1,4 @@
-import { getJobs } from "./jobsAPI";
+import { getJobs, postJob } from "./jobsAPI";
 
 const initialState = {
     jobs: [],
@@ -7,7 +7,27 @@ const initialState = {
     error: ""
 };
 
+//thunk function to fetch jobs
 export const fetchJobs = async () => {
     const jobs = await getJobs();
     return jobs;
 };
+
+//thunk function to add job
+export const addJobs = async (jobData) => {
+    const addedJob = await postJob(jobData);
+    return addedJob;
+};
+
+//thunk function to edit job
+export const editJobs = async ({ id, jobData }) => {
+    const updatedJob = await postJob(id, jobData);
+    return updatedJob;
+};
+
+//thunk function to remove job
+export const removeJobs = async (id) => {
+    const removedJob = await postJob(id);
+    return removedJob;
+};
+
