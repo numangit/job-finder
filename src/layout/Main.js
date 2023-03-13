0b068786296cbde2137ebe326ff8d4f3ad18744b
numangit <abdulnumanjob@gmail.com>
2023-03-13 22:20:15 +0600

    react router dom added and setup implemented

---
+++ src/layout/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/common/Navbar';
+import Sidebar from '../components/common/Sidebar';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar />
+            <div class="max-w-[90rem] mx-auto px-4 sm:px-6 md:px-8 ">
+                <Sidebar />
+                <Outlet />
+            </div>
+        </div>
+    );
+};
+
+export default Main;
