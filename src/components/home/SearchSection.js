494af4b6feea346c9e682985aa5bbe822e4b4151
numangit <abdulnumanjob@gmail.com>
2023-03-13 22:45:42 +0600

    search component added

---
+++ src/components/home/SearchSection.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SearchSection = () => {
+    return (
+        <div class="md:flex space-y-2 md:space-y-0 justify-between mb-10 ">
+            <h1 class="section-title">All Available Jobs</h1>
+            <div class="flex gap-4">
+                <div class="search-field group flex-1">
+                    <i class="fa-solid fa-magnifying-glass search-icon group-focus-within:text-blue-500"></i>
+                    <input type="text" placeholder="Search Job" class="search-input" id="lws-searchJob" />
+                </div>
+                <select id="lws-sort" name="sort" autocomplete="sort" class="flex-1">
+                    <option>Default</option>
+                    <option>Salary (Low to High)</option>
+                    <option>Salary (High to Low)</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default SearchSection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SearchSection from '../components/home/SearchSection';
 
 const Home = () => {
     return (
         <div class="lg:pl-[14rem]  mt-[5.8125rem]">
             <main class="max-w-3xl rounded-lg  mx-auto relative z-20 p-10 xl:max-w-none bg-[#1E293B]">
-                <div class="md:flex space-y-2 md:space-y-0 justify-between mb-10 ">
+                {/* <div class="md:flex space-y-2 md:space-y-0 justify-between mb-10 ">
                     <h1 class="section-title">All Available Jobs</h1>
                     <div class="flex gap-4">
                         <div class="search-field group flex-1">
@@ -17,7 +18,8 @@ const Home = () => {
                             <option>Salary (High to Low)</option>
                         </select>
                     </div>
-                </div>
+                </div> */}
+                <SearchSection />
 
                 <div class="jobs-list ">
                     {/* <!-- Single Job 1--> */}
