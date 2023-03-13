9a354ac8a6616f38915c33f2acedc60649f9d9bf
numangit <abdulnumanjob@gmail.com>
2023-03-13 23:19:32 +0600

    fetch job thunk function added

--- src/features/jobs/jobsSlice.js
+++ src/features/jobs/jobsSlice.js
@@ -1,6 +1,13 @@
+import { getJobs } from "./jobsAPI";
+
 const initialState = {
     jobs: [],
     isLoading: false,
     isError: false,
     error: ""
+};
+
+export const fetchJobs = async () => {
+    const jobs = await getJobs();
+    return jobs;
 };
